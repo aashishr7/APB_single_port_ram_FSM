79d3e0240bdca8212657c898a19ffa14142e4d06
AASHISH RANJAN <44123630+aashishr7@users.noreply.github.com>
2025-05-11 16:52:25 +0530

    Add files via upload

---
+++ package.sv
@@ -0,0 +1,12 @@
+package ram_env_pkg;
+
+`include "seq_item.sv"
+`include "uvm_sequence.sv"
+`include "uvm_sequencer.sv"
+`include "uvm_driver.sv"
+`include "uvm_monitor.sv"
+`include "uvm_scb.sv"
+`include "uvm_agent.sv"
+`include "uvm_env.sv"
+
+endpackage
